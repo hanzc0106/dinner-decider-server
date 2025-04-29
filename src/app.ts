bdfd473c37351c44bb46624c4427fc5c506d8dd3
hanzc0106 <1297411677@qq.com>
2025-04-29 18:49:24 +0800

    feat: 取消后端服务跨域

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Koa from 'koa'
 import http from 'http'
-import cors from '@koa/cors'
+// import cors from '@koa/cors'
 import koaBody from 'koa-body'
 import { getIpAddress } from '@/utils/util'
 import { loggerMiddleware } from '@/log/log'
@@ -11,17 +11,17 @@ import { errorHandler, responseHandler } from '@/middleware/response'
 // console.log(process.env)
 
 const app = new Koa()
-
+console.log('app', app)
 // 配置跨域中间件
-app.use(
-  cors({
-    origin: '*', // 允许所有来源访问，生产环境中应该设置为特定域名
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    credentials: true, // 允许发送cookie
-    maxAge: 86400 // 预检请求有效期，单位为秒
-  })
-)
+// app.use(
+//   cors({
+//     origin: '*', // 允许所有来源访问，生产环境中应该设置为特定域名
+//     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+//     credentials: true, // 允许发送cookie
+//     maxAge: 86400 // 预检请求有效期，单位为秒
+//   })
+// )
 
 // log middleware
 app.use(loggerMiddleware)
